38c5e0af5369c8afd4bd4ebdc145bac48bcf64ea
jsip <jsimonroyer@gmail.com>
2025-01-25 12:48:56 -0500

    feat: update toast dark theme

--- client/src/lib/components/utils/Toast.svelte
+++ client/src/lib/components/utils/Toast.svelte
@@ -26,7 +26,7 @@
 	<div aria-live="assertive" class="pointer-events-none fixed inset-x-0 bottom-6 mb-12">
 		<div class="absolute z-50 flex w-full flex-col items-center">
 			<div
-				class="animate-new pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black/5"
+				class="animate-new pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white dark:bg-slate-700 shadow-lg ring-1 ring-black/5"
 				in:slide={{ duration: 300 }}
 				out:fade={{ duration: 300 }}
 			>
@@ -64,13 +64,13 @@
 							{/if}
 						</div>
 						<div class="ml-3 w-0 flex-1 pt-0.5">
-							<p class="text-sm font-medium text-gray-900">{message}</p>
+							<p class="text-sm font-medium text-gray-900 dark:text-white">{message}</p>
 						</div>
 						<div class="ml-4 flex shrink-0">
 							<button
 								type="button"
 								on:click={dismiss}
-								class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+								class="inline-flex rounded-md bg-white dark:bg-slate-700 dark:text-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 							>
 								<span class="sr-only">Close</span>
 								<svg
